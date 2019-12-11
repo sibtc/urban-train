e9fd02f6340ecd76a613bccbd12d183ce2fb87e2
Luciano da Silva Martins <zicadopv@gmail.com>
2019-12-11 15:13:51 -0300

    adjusts folder TEMPLATES

--- urban_train/settings.py
+++ urban_train/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'urban_train.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
